718ff7cef83bdda2d46903ad1e2fedd2ca5304fb
inaimathi <inaimathi@hermaeus.gateway.2wire.net>
2012-05-04 23:25:04 -0400

    -working gen_server-based generator -still need to decide how I'm handling the call-tree for requirements

--- ps_bc.erl
+++ ps_bc.erl
@@ -1,51 +1,45 @@
 -module(ps_bc).
--export([start/1, stop/0]).
+
+-behaviour(gen_server).
+
+-export([start/0, stop/0]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+	 terminate/2, code_change/3]).
+
 -export([help/0, help/1, write/2, change/1]).
 
-start(TableId) ->
-    spawn(fun() ->
-		  register(barcode, self()),
-		  loop(TableId)
-	  end).
-stop() -> send(stop).
+help() -> gen_server:call(?MODULE, help).
+help(BarcodeType) -> gen_server:call(?MODULE, {help, BarcodeType}).
+write(BarcodeType, Data) -> gen_server:call(?MODULE, {write, BarcodeType, Data}).
+change(TableId) -> gen_server:call(?MODULE, {change, TableId}).    
 
-help() -> send(help).
-help(BarcodeType) -> send({help, BarcodeType}).
-write(BarcodeType, Data) -> send({write, BarcodeType, Data}).
-change(TableId) -> send({change, TableId}).    
+handle_call(help, _From, State) ->
+    {reply, ets:match(State, {'$1', encoder, '_', '_', '_', '_'}), State};
+handle_call({help, BarcodeType}, _From, State) ->
+    {reply, ets:match(State, {BarcodeType, encoder, '_', '$2', '_', '_'}), State};
+handle_call({write, BarcodeType, Data}, _From, State) ->
+    Fname = string:strip(os:cmd("mktemp"), right, $\n),
+    {ok, File} = file:open(Fname, [append]),
+    [[{def_arg, ExArgs}, Body]] = ets:match(State, {BarcodeType, encoder, '_', '_', '$1', '$2'}),
+    [[Pre]] = ets:match(State, {preamble, '$1'}),
+    file:write(File, "%!PS-Adobe-2.0\n%%BoundingBox: 0 0 200 200\n%%LanguageLevel: 2\n"),
+    file:write(File, Pre),
+    file:write(File, "\n/Helvetica findfont 10 scalefont setfont\n"),
+    file:write(File, Body),
+    io:format(File, "10 10 moveto (~s) (~s) /~s /uk.co.terryburton.bwipp findresource exec showpage",
+	      [Data, string:join(ExArgs, " "), BarcodeType]),
+    file:close(File),
+    {reply, Fname, State};
+handle_call({change_table, Tab}, _From, _State) ->
+    {reply, {watching_table, Tab}, Tab}.
 
-send(Msg) -> 
-    barcode ! {self(), Msg},
-    receive 
-	Result -> Result
-    end.
+%%%%%%%%%%%%%%%%%%%% generic actions
+start() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []). %% Name, Mod, InitArgs, Opts
+stop() -> gen_server:call(?MODULE, stop).
 
-loop(TableId) ->
-    receive
-	{Pid, help} -> 
-	    Pid ! ets:match(TableId, {'$1', encoder, '_', '_', '_', '_'}),
-	    loop(TableId);
-	{Pid, {help, BarcodeType}} -> 
-	    Pid ! ets:match(TableId, {BarcodeType, encoder, '_', '$2', '_', '_'}), 
-	    loop(TableId);
-	{Pid, {write, BarcodeType, Data}} -> 
-	    Fname = string:strip(os:cmd("mktemp"), right, $\n),
-	    {ok, File} = file:open(Fname, [append]),
-	    [[{def_arg, ExArgs}, Body]] = ets:match(TableId, {BarcodeType, encoder, '_', '_', '$1', '$2'}),
-	    [[Pre]] = ets:match(TableId, {preamble, '$1'}),
-	    file:write(File, "%!PS-Adobe-2.0\n%%BoundingBox: 0 0 200 200\n%%LanguageLevel: 2\n"),
-	    file:write(File, Pre),
-	    file:write(File, "\n/Helvetica findfont 10 scalefont setfont\n"),
-	    file:write(File, Body),
-	    io:format(File, "10 10 moveto (~s) (~s) /~s /uk.co.terryburton.bwipp findresource exec showpage",
-		      [Data, string:join(ExArgs, " "), BarcodeType]),
-	    file:close(File),
-	    Pid ! Fname,
-	    loop(TableId);
-	
-	{Pid, {change, Tab}} -> 
-	    Pid ! Tab, 
-	    loop(Tab);
-	{Pid, stop} -> 
-	    Pid ! stopping
-    end.
+%%%%%%%%%%%%%%%%%%%% gen_server handlers
+init([]) -> {ok, barcode_data:import_ets_file()}.
+handle_cast(_Msg, State) -> {noreply, State}.
+handle_info(_Info, State) -> {noreply, State}.
+terminate(_Reason, _State) -> ok.
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
