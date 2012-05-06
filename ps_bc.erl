89fc9e5b81b1a5c87795d33f08ef898f45af1cf1
inaimathi <inaimathi@hermaeus.gateway.2wire.net>
2012-05-06 00:52:26 -0400

    -fully functioning writer (tested the ets lookups at write-time, turns out it's not nearly enough of a delay to make me try caching out all the renderers and requirements)

--- ps_bc.erl
+++ ps_bc.erl
@@ -13,6 +13,21 @@ help(BarcodeType) -> gen_server:call(?MODULE, {help, BarcodeType}).
 write(BarcodeType, Data) -> gen_server:call(?MODULE, {write, BarcodeType, Data}).
 change(TableId) -> gen_server:call(?MODULE, {change, TableId}).    
 
+
+write_component(preamble, Table, File) ->
+    [[Pre]] = ets:match(Table, {preamble, '$1'}),
+    file:write(File, Pre);
+write_component(Name, Table, File) -> 
+    file:write(File, lookup_component(Name, Table)).
+		     
+lookup_component(Name, Table) -> 
+    Ren = ets:match(Table, {Name, renderer, '$1'}),
+    Enc = ets:match(Table, {Name, encoder, '_', '_', '_', '$1'}),
+    case {Ren, Enc} of
+	{[], [[Res]]} -> Res;
+	{[[Res]], []} -> Res
+    end.	    
+
 handle_call(help, _From, State) ->
     {reply, ets:match(State, {'$1', encoder, '_', '_', '_', '_'}), State};
 handle_call({help, BarcodeType}, _From, State) ->
@@ -20,12 +35,12 @@ handle_call({help, BarcodeType}, _From, State) ->
 handle_call({write, BarcodeType, Data}, _From, State) ->
     Fname = string:strip(os:cmd("mktemp"), right, $\n),
     {ok, File} = file:open(Fname, [append]),
-    [[{def_arg, ExArgs}, Body]] = ets:match(State, {BarcodeType, encoder, '_', '_', '$1', '$2'}),
-    [[Pre]] = ets:match(State, {preamble, '$1'}),
+    [[{requires, CompList}, {def_arg, ExArgs}]] = ets:match(State, {BarcodeType, encoder, '$1', '_', '$2', '_'}),
     file:write(File, "%!PS-Adobe-2.0\n%%BoundingBox: 0 0 200 200\n%%LanguageLevel: 2\n"),
-    file:write(File, Pre),
+    write_component(preamble, State, File),
     file:write(File, "\n/Helvetica findfont 10 scalefont setfont\n"),
-    file:write(File, Body),
+    lists:map(fun (C) -> write_component(C, State, File) end, CompList),
+    write_component(BarcodeType, State, File),
     io:format(File, "10 10 moveto (~s) (~s) /~s /uk.co.terryburton.bwipp findresource exec showpage",
 	      [Data, string:join(ExArgs, " "), BarcodeType]),
     file:close(File),
@@ -34,7 +49,7 @@ handle_call({change_table, Tab}, _From, _State) ->
     {reply, {watching_table, Tab}, Tab}.
 
 %%%%%%%%%%%%%%%%%%%% generic actions
-start() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []). %% Name, Mod, InitArgs, Opts
+start() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []). %% {local/global, Name}, Mod, InitArgs, Opts
 stop() -> gen_server:call(?MODULE, stop).
 
 %%%%%%%%%%%%%%%%%%%% gen_server handlers
